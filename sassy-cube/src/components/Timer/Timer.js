4f5a6b43bca5c39a6b8121933217f61ae19f5ad7
Rhiannon <raesalceda@gmail.com>
2019-09-21 21:26:31 -0700

    Unique sayings with increasing spicy levels

--- sassy-cube/src/components/Timer/Timer.js
+++ sassy-cube/src/components/Timer/Timer.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import Speech from 'speak-tts';
+import {
+  mild, medium, spicy
+} from '../../constants';
 
 const TimerWrapper = styled.div`
   font-size: 48px;
@@ -21,7 +24,12 @@ class Timer extends Component {
     
     constructor (props) {
       super(props)
-      this.state = {count: 1}
+      this.state = {
+        count: 1, 
+        mildInit: mild,
+        medInit: medium,
+        spicyInit: spicy
+      }
     }
 
     componentWillUnmount () {
@@ -29,10 +37,72 @@ class Timer extends Component {
     }
 
     tick () {
-      if(this.state.count % 10 === 0) {
-        triggerSpeak('Oh... You couldn\'t solve me? ... How useless.')
+      // if(this.state.count % 10 === 0) {
+      //   triggerSpeak('Oh... You couldn\'t solve me? ... How useless.') //RETURN
+      // }
+      if(this.state.count % 15 === 0) {
+        this.pickMessage(this.state.count)
       }
+      
       this.setState({count: (this.state.count + 1)})
+    }
+
+    pickMessage() {
+      let randomIndex = 0
+      if(this.state.count <= 90) {
+        //pull from mild
+        randomIndex = Math.floor(Math.random() * this.state.mildInit.length)
+        triggerSpeak(this.state.mildInit[randomIndex])
+
+        const tempArray = this.state.mildInit
+        tempArray.splice(randomIndex, 1)
+
+
+        this.setState({
+          mildInit: tempArray
+        })
+
+       // console.log(this.state.mildInit)
+      }
+
+      else if(this.state.count <= 180) {
+        //pull from medium
+        randomIndex = Math.floor(Math.random() * this.state.medInit.length)
+        triggerSpeak(this.state.medInit[randomIndex])
+
+        const tempArray = this.state.medInit
+        tempArray.splice(randomIndex, 1)
+
+
+        this.setState({
+          medInit: tempArray
+        })
+
+       // console.log(this.state.medInit)
+      }
+
+      else if(this.state.count > 180) {
+
+        if(this.state.spicyInit.length === 0)
+          this.setState({
+            spicyInit: spicy
+          })
+          
+        //pull from spicy
+        randomIndex = Math.floor(Math.random() * this.state.spicyInit.length)
+        triggerSpeak(this.state.spicyInit[randomIndex])
+
+        const tempArray = this.state.spicyInit
+        tempArray.splice(randomIndex, 1)
+
+
+        this.setState({
+          spicyInit: tempArray
+        })
+        
+       // console.log(this.state.spicyInit)
+      }
+
     }
 
     startTimer () {
---
+++ sassy-cube/src/constants.js
@@ -0,0 +1,32 @@
+//one voice every 15 seconds
+//step up every minute and a half
+//every two minutes, pull a random quote
+
+export const mild = [
+    'Are you distracted?',
+    'Surely you can solve me quickly.', 
+    'This shouldn\'t be that hard.', 
+    'I\'m baffled. Truly.', 
+    'You\'ve solved puzzles before, right?', 
+    'Wow. Just wow.'
+]
+
+export const medium = [
+    'Oh... You still haven\'t solve me? ... How useless.',
+    'You don\'t know what you\'re doing, do you.',
+    'My faith is slowly dwindling.', 
+    'I\'ve seen fourth graders solve us quicker.',
+    'Now would be the time to ask for assistance.', 
+    'You\'re really good at this. You should consider doing this for a living. Slash s.', 
+    'Don\'t quit your day job.'
+]
+
+export const spicy = [
+    'I don\'t know what\'s cuter: that you\'re still trying, or that I\'m still humoring you.',
+    'Should I pull up a tutorial for you? You clearly need it.', 
+    'Are you really trying? I\'ve seen better efforts from sloths.', 
+    'Perhaps this is too hard for you. Unfortunately, I can\'t downgrade myself to suit your needs.', 
+    'If you take this long to solve a Rubix Cube, I\'d hate to see you tie your shoes.', 
+    'I got so bored waiting on you, I actually read the entire literary canon of the human race. Ugh. I hope YOU didn\'t write any of them. ', //Glados
+    'I strongly encourage you reflect on this moment and what it says about your abilities.'
+]
